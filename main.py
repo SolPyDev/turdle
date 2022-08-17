6fdeeb2d595fe36956562131888c62223dbfca7b
SolPyDev <108149462+SolPyDev@users.noreply.github.com>
2022-08-17 15:11:41 -0400

    Add requirements.txt

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from art import tprint
 from turdle_pkg import menu
 
-# Turdle Welcome Screen
-tprint("Turdle", font="tarty1")
-print("                           The Shitty Wordle Clone\n")
+if __name__ == "__main__":
+    # Turdle Welcome Screen
+    tprint("Turdle", font="tarty1")
+    print("                           The Shitty Wordle Clone\n")
 
-# Display Main Menu
-menu.main_menu()
+    # Display Main Menu
+    menu.main_menu()
+
